2eabfb92988e028cf50a607896fdaa062c2a216e
Angeli K <56638770+angeli-sk@users.noreply.github.com>
2020-10-21 13:20:02 +0200

    Add files via upload
    colors pls add

---
+++ colors.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   colors.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: akramp <akramp@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/09/22 08:23:26 by akramp        #+#    #+#                 */
+/*   Updated: 2020/09/22 08:24:57 by akramp        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define COLOR_RESET "\033[37m"
+# define COLOR_RED "\033[38;5;196m"
+# define COLOR_GREEN "\033[38;5;46m"
+# define COLOR_BLUE "\033[38;5;51m"
+# define COLOR_BLU "\033[38;5;152m"
+# define COLOR_ORANGE "\033[38;5;214m"
+# define COLOR_PURPLE "\033[38;5;98m"
+# define COLOR_YELLOW "\033[38;5;220m"
+# define COLOR_PINK "\033[38;5;205m"
+# define BOLD "\e[1m"
+# define ITALIC "\e[3m"
+# define FORMAT_RESET "\e[0m"
+
+#endif
