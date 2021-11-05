6a6b38f98fd4b5ddd4ac8816224fb2858b04dd97
angeli-sk <akramp@f1r4s9.codam.nl>
2021-11-05 11:21:26 +0100

    more colors

--- colors.h
+++ colors.h
@@ -6,7 +6,7 @@
 /*   By: akramp <akramp@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/22 08:23:26 by akramp        #+#    #+#                 */
-/*   Updated: 2020/10/21 13:34:33 by akramp        ########   odam.nl         */
+/*   Updated: 2021/11/05 11:21:01 by akramp        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,23 @@
 # define COLOR_PURPLE	"\033[38;5;98m"
 # define COLOR_YELLOW	"\033[38;5;220m"
 # define COLOR_PINK		"\033[38;5;205m"
+
+# define COLOR_BLACK	"\033[0;30m"
+# define COLOR_GREY		"\033[0;90m"
+# define COLOR_DARKPINK	"\033[0;31m"
+# define COLOR_SALMON	"\033[0;91m"
+# define COLOR_NEON		"\033[0;32m"
+# define COLOR_MATCHA	"\033[0;92m"
+# define COLOR_SUNYEL	"\033[0;33m"
+# define COLOR_LIGHTYE	"\033[0;93m"
+# define COLOR_LIGHTPUR	"\033[0;34m"
+# define COLOR_BABYBLUE	"\033[0;94m"
+# define COLOR_HOTPINK	"\033[0;35m"
+# define COLOR_BABYPINK	"\033[0;95m"
+# define COLOR_CYAN		"\033[0;36m"
+# define COLOR_LIGHTBLU	"\033[0;96m"
+# define COLOR_OFFWHITE	"\033[0;37m"
+# define COLOR_WHITE	"\033[0;97m"
 
 /*
 ** Inverted colors :0
